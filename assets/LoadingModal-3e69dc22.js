870589473e03d0d591c9629105d40f585d1ae4c1
Mootassam <Mootassame@gmail.com>
2024-07-24 11:33:55 +0100

    add the new commit

---
+++ assets/LoadingModal-3e69dc22.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-e5203b7d.js";function i(){return s.jsx("div",{className:"loading__page",children:s.jsx("div",{className:"spinner__big"})})}export{i as L};
---
+++ assets/Search-045992d3.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-e5203b7d.js";function a(){return e.jsx("div",{className:"search__page",children:e.jsx("div",{className:"search__input",children:e.jsx("input",{type:"text",placeholder:"search"})})})}export{a as default};
